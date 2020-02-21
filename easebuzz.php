7332e463a0b6087864354714f22f9b5fdf9affe3
Pavan Jadhav <pavan@lsssoftware.com>
2020-02-21 16:16:56 +0530

    Updated PHP Kit

--- easebuzz.php
+++ easebuzz.php
@@ -53,9 +53,9 @@
         /*
         * Based on API call change the Merchant key and salt key for testing(initiate payment).
         */
-        $MERCHANT_KEY = "N89RDXN7JT";
-        $SALT = "9M0AFW7HAQ";
-       // $ENV = "dev";    // setup test enviroment (testpay.easebuzz.in).
+        $MERCHANT_KEY = "xxxxxx";
+        $SALT = "xxxxxx";
+       // $ENV = "test";    // setup test enviroment (testpay.easebuzz.in).
         $ENV = "prod";   // setup production enviroment (pay.easebuzz.in).
  
         $easebuzzObj = new Easebuzz($MERCHANT_KEY, $SALT, $ENV);
